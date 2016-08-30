2d10f96478b28bb6aae062e84386cdf28ae23f4e
Mario Tressa Juzar <mariotj.tj@gmail.com>
2016-08-30 10:11:52 +0700

    diahalay

--- hapus_ujian.php
+++ hapus_ujian.php
@@ -11,10 +11,38 @@
 	if ($there!=null){
 		$judul = $there['judul_ujian'];
 		
-		$delete = "DELETE FROM `info_ujian` WHERE `id_ujian`='$id' ";
-		$delete_query = mysqli_query($link, $delete);
+		$ini = "SELECT * FROM `soal` WHERE `id_ujian`='$id'";
+		$iniquery = mysqli_query($link, $ini);
+		
+		while ($data = mysqli_fetch_array($iniquery)){
+			$idsoal = $data['id_soal'];
+			
+			// periksa pilihan_jawaban
+			$cekdulu = mysqli_query($link, "SELECT * FROM pilihan_jawaban WHERE id_soal='$idsoal'");
+			if ($cekdulu){
+				$delete = "DELETE FROM `pilihan_jawaban` WHERE `id_soal`='$idsoal' ";
+				$delete_query = mysqli_query($link, $delete);
+			}
+			
+			// periksa jawaban_user
+			$cekdulu = mysqli_query($link, "SELECT * FROM jawaban_user WHERE id_ujian='$id'");
+			if ($cekdulu){
+				$delete = "DELETE FROM `jawaban_user` WHERE `id_ujian`='$idsoal' ";
+				$delete_query = mysqli_query($link, $delete);
+			}
+			
+			// periksa laporan_ujian_guru
+			$cekdulu = mysqli_query($link, "SELECT * FROM laporan_ujian_guru WHERE id_ujian='$id'");
+			if ($cekdulu){
+				$delete = "DELETE FROM `laporan_ujian_guru` WHERE `id_ujian`='$idsoal' ";
+				$delete_query = mysqli_query($link, $delete);
+			}
+		}
 		
 		$delete = "DELETE FROM `soal` WHERE `id_ujian`='$id' ";
+		$delete_query = mysqli_query($link, $delete);
+		
+		$delete = "DELETE FROM `info_ujian` WHERE `id_ujian`='$id' ";
 		$delete_query = mysqli_query($link, $delete);
 
 		if($delete_query){
