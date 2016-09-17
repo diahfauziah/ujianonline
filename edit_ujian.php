43eb2b3c80e3d3b1f376162ca8b9b9583be65e6e
Mario Tressa Juzar <mariotj.tj@gmail.com>
2016-09-17 13:26:01 +0700

    diah

--- edit_ujian.php
+++ edit_ujian.php
@@ -542,7 +542,6 @@
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/tooltipsy.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/bootstrap-tagsinput.min.js"></script>
 
     <!-- Include JS files. -->
     <script type="text/javascript" src="froala/js/froala_editor.min.js"></script>
